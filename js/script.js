a1d7bf0dd8556520c13f74e852245738474324e9
Andreaserra1997 <serra.andrea1997@gmail.com>
2023-04-27 15:12:35 +0200

    add countdown

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let countDownDate = new Date ("Apr 28, 2023 09:30:00").getTime();
+let x = setInterval(function() {
+    let now = new Date().getTime();
+    let distance = countDownDate - now;
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.querySelector('#time').innerHTML = days + ' Giorni ' + hours + ' Ore ' + minutes + ' Minuti ' + seconds + ' Secondi ';
+
+    if (distance = 0) {
+        clearInterval(x);
+        document.querySelector('#time').innerHTM = 'Il tempo è scaduto!!!'
+    }
+},1000);
